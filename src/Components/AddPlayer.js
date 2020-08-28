c7f7db197c7c789b40ee311eef83fcb16b443459
Arian Rahbar <arahbar@usc.edu>
2020-08-28 01:35:52 -0500

    utc final

--- src/Components/AddPlayer.js
+++ src/Components/AddPlayer.js
@@ -1,36 +1,9 @@
-import React, {useState} from 'react'
-import DatePicker from 'react-datepicker'
+import React from 'react'
 import "react-datepicker/dist/react-datepicker.css";
 import './AddPlayer.css'
-import { getTimes, roundTime, dateToInterval, intervalArray } from '../helpers/timeHelper'
 
 // Form for booking or unbooking a court
 function AddPlayer(props) {
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const endTime  = getEnd(start, length.value)
-        const intervals = intervalArray(dateToInterval(start), dateToInterval(endTime))
-        const formData = {
-            court: courtNumber.valueAsNumber,
-            date: start.getDate(),
-            intervals
-        }
-        props.book(formData)
-    }
-    const [start, setStart] = useState(roundTime(new Date()))
-    const [minStartDate, maxStartDate, minStartTime, maxStartTime] = getTimes(start)
-    let courtNumber = 0;
-    let length = 0;
-
-    const success = (data) => {
-        console.log(data)
-    }
-
-    const failure = data => {
-        console.log(data)
-    }
-
-
     return (
         <div>
             {!props.booked ?  props.selected ? 
