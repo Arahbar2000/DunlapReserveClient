e553ed1d2192c6eefd8fa439eba8a4478c23f7c0
Arian Rahbar <arahbar@usc.edu>
2020-08-30 02:41:29 -0500

    eh

--- src/Context/authContext.js
+++ src/Context/authContext.js
@@ -4,37 +4,6 @@ import { signup } from '../helpers/userHelpers'
 import { useGoogleLogin, useGoogleLogout } from 'react-google-login'
 const { REACT_APP_GOOGLE_CLIENT_ID } = process.env
 const AuthContext = createContext()
-const initialState = {
-    isAuthenticated: false,
-    user: null,
-    token: null,
-}
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "LOGIN":
-            return {
-                ...state,
-                isAuthenitcated: true, 
-                user: action.payload.user
-            }
-        case "LOGOUT":
-            return {
-                ...state, 
-                isAuthenitcated: false, 
-                user: null
-            }
-        case "ERROR":
-            return {
-                ...state, 
-                isAuthenticated: false, 
-                error: action.payload.error, 
-                user: null
-            }
-    }
-}
-
-
 
 const AuthProvider = props => {
     const [data, setData] = useState({auth: false, token: null, user: null})
@@ -63,7 +32,7 @@ const AuthProvider = props => {
     const { signOut } = useGoogleLogout({
         clientId: REACT_APP_GOOGLE_CLIENT_ID,
         onLogoutSuccess: handleLogoutSuccess,
-        onFailure: () => setData({ ...data, auth: false, token: null, user: null })
+        onFailure: handleLogoutFailure
     })
 
     if(!loaded) {
